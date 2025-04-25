e191abe4f3da874cf39d39113ffd389a2f08010c
VitóriaN27 <vitoresksantos005@gmail.com>
2025-04-25 11:36:30 -0300

    gerenciamento de nota

---
+++ condicional.py
@@ -0,0 +1,14 @@
+nota = 7
+frequência = 90
+
+
+if nota >= 7 and frequência > 70:
+   print("Aprovado")
+else:
+   print("Reprovado")
+
+
+if nota >= 7 or frequência > 70:
+   print("Aprovado")
+else:
+   print("reprovado")

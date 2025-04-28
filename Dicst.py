3088eefc4be2a9793eadf13603b8d0566db760dd
VitóriaN27 <vitoresksantos005@gmail.com>
2025-04-28 12:56:59 -0300

    Dicionarios, setas e tuplas

--- Dicst.py
+++ Dicst.py
@@ -38,7 +38,7 @@ print(tupla)
 tupla[4]
 
 dic2 = {(0, 1): 0 , (1, 2): 1, (2, 3): 2, (3, 4): 3, (4, 5): 4, (5, 6): 5 , (6, 7): 6,  (7, 8 ): 7, (8, 9 ): 8,  (9, 10 ): 9}
-dic2
+print(dic2)
 
 print(type(notas))
 print(type(bigdata))

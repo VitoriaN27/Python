6d533a9daa2d7cd7b72ce9932a564026c428116a
VitóriaN27 <vitoresksantos005@gmail.com>
2025-04-28 11:10:54 -0300

    inputpy

---
+++ input.py
@@ -0,0 +1,21 @@
+print("informe o valor: ")
+i = input()
+print(type(i))
+
+print("Informe o valor: ")
+i = input()
+i = input("Informe o valor: ")
+
+
+var = input("Informe o valor: ")
+print(type(var))
+var = int(var)
+print(type(var))
+
+var = input("coloca o valor: ")
+print(type(var))
+var = float(var)
+print(type(var))
+
+
+

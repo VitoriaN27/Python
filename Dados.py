73aa4e985ccc2c8fdbd6c074452e8a19f4ab92ef
VitóriaN27 <vitoresksantos005@gmail.com>
2025-04-28 11:11:23 -0300

    dadospy

--- Dados.py
+++ Dados.py
@@ -13,3 +13,17 @@ w = True
 y = False
 
 print(y)
+
+x = 10
+y = 20
+z = 100
+w = (x + y) * z / 100
+
+print(w)
+
+print("Este texto será impresso no console")
+print(x)
+print("Textp e duas variáveis: ", x , ", ", z)
+
+print(type(x))
+print(type(m))
